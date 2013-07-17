f3a09d453b3cb5d0d9a8648add72a9faf395b905
Daniel C. Sobral <dcsobral@gmail.com>
2013-07-17 03:28:46 -0300

    Improved exception handling

--- src/main/java/com/richrelevance/stash/plugin/PullRequestHook.java
+++ src/main/java/com/richrelevance/stash/plugin/PullRequestHook.java
@@ -8,18 +8,23 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.HttpURLConnection;
-import java.net.URLEncoder;
+import java.net.*;
+import static java.net.HttpURLConnection.HTTP_OK;
 
 public class PullRequestHook {
+  // add log4j.logger.attlassian.plugin=DEBUG  to stash-config.properties on Stash home directory to use this logger
+  // private static final Logger log = LoggerFactory.getLogger("atlassian.plugin");
 
+  // Needs a log4j.properties
   private static final Logger log = LoggerFactory.getLogger(PullRequestHook.class);
-  private String BASEURL = System.getProperty("prhook.baseUrl", "http://localhost/bamboo");
-  private String URL = System.getProperty("prhook.url", "$BASEURL/rest/api/latest/queue/$PLAN?bamboo.variable.prnumber=$PRNUMBER&os_authType=basic");
-  private String USER = System.getProperty("prhook.user", "user");
-  private String PASSWORD = System.getProperty("prhook.password", "password");
+
+  private final String BASEURL = System.getProperty("prhook.baseUrl", "http://localhost/bamboo");
+  private final String URL = System.getProperty("prhook.url", "$BASEURL/rest/api/latest/queue/$PLAN?bamboo.variable.prnumber=$PRNUMBER&os_authType=basic");
+  private final String USER = System.getProperty("prhook.user", "user");
+  private final String PASSWORD = System.getProperty("prhook.password", "password");
 
   @EventListener
   public void onPullRequestOpen(PullRequestOpenedEvent event) {
@@ -32,36 +37,95 @@ public class PullRequestHook {
   }
 
   @EventListener
-  public void onPullRequestRescope(PullRequestRescopedEvent event) { }
+  public void onPullRequestRescope(PullRequestRescopedEvent event) {
+  }
 
   @EventListener
-  public void onPullRequestComment(PullRequestCommentAddedEvent event) {}
+  public void onPullRequestComment(PullRequestCommentAddedEvent event) {
+  }
 
   private void triggerPullRequest(PullRequest pullRequest) {
-    String url = getUrl(pullRequest);
-    String authStringEnc = getAuthenticationString();
-    try {
-      HttpURLConnection connection = (HttpURLConnection) new java.net.URL(url).openConnection();
+    final String url = getUrl(pullRequest);
+    if (url != null && !url.isEmpty()) {
+      String authStringEnc = getAuthenticationString();
+
+      final HttpURLConnection connection;
+      try {
+        URLConnection conn = new java.net.URL(url).openConnection();
+        if (conn instanceof HttpURLConnection) {
+          connection = (HttpURLConnection) conn;
+        } else {
+          log.error("not an Http connection: " + url);
+          return;
+        }
+      } catch (IOException e) {
+        log.error("unable to open a connection to " + url, e);
+        return;
+      }
+
       connection.setRequestProperty("Authorization", "Basic " + authStringEnc);
       connection.setRequestProperty("Accept", "application/json");
-      connection.setRequestMethod("POST");
-      InputStream buildRequisition = connection.getInputStream();
-      String response = IOUtils.toString(buildRequisition, "UTF-8");
-      log.info("Build Trigger Response: " + response);
-      buildRequisition.close();
-    } catch (Exception e) {
-      log.error("Error triggering: " + url, e);
+      connection.setRequestProperty("Accept-Charset", "UTF-8");
+
+      try {
+        connection.setRequestMethod("POST");
+      } catch (ProtocolException e) {
+        log.error("unable to set method to POST", e);
+        return;
+      }
+
+      try {
+        connection.connect();
+      } catch (SocketTimeoutException e) {
+        log.error("timeout connecting to " + url, e);
+      } catch (IOException e) {
+        log.error("unable to connect to " + url, e);
+      }
+
+      try {
+        final int responseCode = connection.getResponseCode();
+
+        if (responseCode == -1) {
+          log.error("response from " + url + "is not a valid http response");
+          return;
+        } else if (responseCode != HTTP_OK) {
+          log.error("failed to trigger " + url + ": " + responseCode + "(" + connection.getResponseMessage() + ")");
+          return;
+        }
+      } catch (IOException e) {
+        log.error("unable to get response code from connection to " + url);
+        return;
+      }
+
+      try {
+        String encoding = connection.getContentEncoding();
+        encoding = encoding == null ? "UTF-8" : encoding;
+
+        final InputStream buildRequisitionResponse = connection.getInputStream();
+        final String response = IOUtils.toString(buildRequisitionResponse, encoding);
+        log.info("response from " + url + ": " + response);
+        buildRequisitionResponse.close();
+      } catch (IOException e) {
+        log.error("unable to get POST response", e);
+      }
+    } else {
+      log.error("empty trigger url");
     }
   }
 
   private String getUrl(PullRequest pullRequest) {
-    Long prNumber = pullRequest.getId();
-    return URL.replace("$BASEURL", BASEURL).replace("$PLAN", urlEncode("RRCORE-PRTEST")).replace("$PRNUMBER", prNumber.toString());
+    final Long prNumber = pullRequest.getId();
+    if (prNumber != null) {
+      return URL.replace("$BASEURL", BASEURL).replace("$PLAN", urlEncode("RRCORE-PRTEST")).replace("$PRNUMBER", prNumber.toString());
+    } else {
+      log.error("id of pull request is null:" + pullRequest);
+      return "";
+    }
   }
 
   private String getAuthenticationString() {
-    String authString = USER + ":" + PASSWORD;
-    byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
+    final String authString = USER + ":" + PASSWORD;
+    final byte[] authEncBytes = Base64.encodeBase64(authString.getBytes());
     return new String(authEncBytes);
   }
 
